10b727f48a938901b512eea933e74ae18b9ab980
Nevin Flanagan <nevin@playsmith.net>
2023-09-17 15:55:07 -0400

    Excluded svg files to avoid committing flamegraphs.

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,80 @@
-use rs_space::Invaders;
-use sdl2::pixels::Color;
+extern crate sdl3;
 
-extern crate sdl2;
+use rs_space::Invaders;
+use std::time::{Instant, Duration};
+use sdl3::{rect::Rect, render::FRect, pixels::{Color, Palette}};
 
 const SCREEN_WIDTH: u32 = 224;
 const SCREEN_HEIGHT: u32 = 256;
+const SCREEN_SCALE: u32 = 4;
 
-struct Frames<F: FnMut() -> u32>(u32, F, u32);
-
-impl<F: FnMut() -> u32> Frames<F> {
-    fn with(gap: u32, f: F) -> Self {
-        Self (0, f, gap)
-    }
-}
-
-impl<F: FnMut() -> u32> Iterator for Frames<F> {
-    type Item = u32;
-    fn next(&mut self) -> Option<Self::Item> {
-        let now = loop {
-            let now = self.1();
-            if now - self.0 >= self.2 {
-                break now;
-            }
-        };
-        let elapsed = now - self.0;
-        self.0 = now;
-        Some(elapsed)
-    }
-}
+const FRAME_LENGTH: Duration = match Duration::from_secs(1).checked_div(60) {None => panic!(), Some(d) => d};
 
 fn main() -> Result<(), Box<(dyn std::error::Error + 'static)>> {
-    use sdl2::pixels::PixelFormatEnum::Index1LSB;
-    let libsdl = sdl2::init()?;
+    let libsdl = sdl3::init()?;
     let video = libsdl.video()?;
-    let time = libsdl.timer()?;
     let mut input = libsdl.event_pump()?;
-    let window = video.window("Space Invaders", SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2)
+    let window = video.window("Space Invaders", SCREEN_WIDTH * SCREEN_SCALE, SCREEN_HEIGHT * SCREEN_SCALE)
         .position_centered()
         .build()?;
     let mut renderer = window.into_canvas()
         .present_vsync()
         .build()?;
-    let scribe = renderer.texture_creator();
+    let _scribe = renderer.texture_creator();
     let mut board = Invaders::new();
     let mut machine = board.install();
-    renderer.set_draw_color(Color::RGB(0, 0, 0));
-    renderer.clear();
-    renderer.present();
-    let mut half = 8;
-    for _elapsed in Frames::with(16, || time.ticks()) {
-        let quit = 'game: loop {
-            for event in input.poll_iter() {
-                use sdl2::event::Event;
-                match event {
-                    Event::Quit {..} => break 'game true,
-                    _ => ()
-                }
-
+    let _colors = Palette::with_colors(
+        &[Color::RGB(0, 0, 0), Color::RGB(255, 255, 255), ]
+    )?;
+    let mut half = 1;
+    let start = Instant::now();
+    let mut interrupted = start;
+    loop {
+        for event in input.poll_iter() {
+            use sdl3::event::Event;
+            match event {
+                Event::Quit {..} => return Ok(()),
+                _ => ()
             }
         };
-        if quit { return Ok(()); }
-        let _ = machine.next();
-        machine.reset_to(half)?;
-        half = 18 - half;
-        renderer.set_draw_color(Color::RGB(0, 0, 0));
-        renderer.clear();
-        let frame = sdl2::surface::Surface::from_data(machine.raster(), SCREEN_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT / 8, Index1LSB)?;
-        let screen = scribe.create_texture_from_surface(frame)?;
-        renderer.copy_ex(&screen, None, None, 90.0, None, false, false)?;
-        renderer.present();
+        #[cfg(debug_assertions)]
+        eprintln!("> {:#06X}", machine.as_ref()[lemurs::support::Internal::ProgramCounter]);
+        #[cfg(debug_assertions)]
+        let _ = machine.next().unwrap()?;
+        #[cfg(not(debug_assertions))]
+        machine.next().unwrap();
+        while interrupted.elapsed() >= FRAME_LENGTH {
+            eprintln!("{interrupted:?}");
+            interrupted += FRAME_LENGTH;
+            let interrupts = machine.reset_to(half)?;
+            if interrupts {
+                half = 3 - half;
+                renderer.set_draw_color(Color::RGB(0, 0, 0));
+                renderer.clear();
+                for (i, byte) in machine.raster().iter().copied().enumerate() {
+                    let i = i as i32;
+                    let mut pixel = Rect::new( SCREEN_SCALE as i32 * i / 32, (SCREEN_SCALE * SCREEN_HEIGHT) as i32 - 8 * (i % 32), SCREEN_SCALE, SCREEN_SCALE);
+                    for shift in 0..8 {
+                        let value = 255 * (byte >> shift & 1) as u8;
+                        renderer.set_draw_color(Color::RGB(value, value, value));
+                        renderer.fill_rect(FRect::from(pixel))?; 
+                    }
+                    pixel.offset(0, -(pixel.height() as i32));
+                }
+            }
+/*
+            let mut frame = sdl2::surface::Surface::from_data(machine.raster(), SCREEN_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT / 8, Index1LSB)?;
+            frame.set_palette(&colors)?;
+            let screen = scribe.create_texture_from_surface(frame)?;
+            let frame = Rect::new(
+                SCREEN_WIDTH as i32 - SCREEN_HEIGHT as i32,
+                SCREEN_HEIGHT as i32 - SCREEN_WIDTH as i32,
+                SCREEN_HEIGHT * 2, 
+                SCREEN_WIDTH * 2,
+            );
+            renderer.copy_ex(&screen, None, frame, 90.0, None, false, false)?;
+ */ 
+            renderer.present();
+        }
     }
-    Ok(())
 }
